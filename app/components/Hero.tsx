43d677fbee097565112e0a2b991093c007a5403f
jay <ronokamau@gmail.com>
2023-07-08 00:46:33 +0300

    Hero component - navbar styling

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,9 +1,12 @@
 "use client"
+import Image from 'next/image';
 import React from 'react';
 import CustomButton from './CustomButton';
 
+
+
 const Hero = () => {
-    const handleClick = () => {
+    const handleScroll = () => {
         console.log('What');
     };
 
@@ -11,7 +14,7 @@ const Hero = () => {
         <div className='hero'>
             <div className="flex-1 pt-36 padding-x">
                 <h1 className="hero__title">
-                    Discover, reserve and rent a car in no time, hassle-free!
+                    Discover, book or rent a car in no time, hassle-free!
                 </h1>
                 <p className="hero__subtitle">
                     Your car rental experience made effortless. Book with ease and hit the road.
@@ -19,9 +22,19 @@ const Hero = () => {
                 <CustomButton
                     title='Explore Cars'
                     containerStyles='bg-indigo-500 text-white rounded-full mt-10'
-                    handleClick={handleClick}
+                    handleClick={handleScroll}
                 />
-
+            </div>
+            <div className='hero__image-container'>
+                <div className='hero__image'>
+                    <Image
+                        src={'/hero.png'}
+                        alt='hero'
+                        fill
+                        className='object-contain'
+                    />
+                    <div className='hero__image-overlay'/>
+                </div>
             </div>
         </div>
     )
